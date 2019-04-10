c6c88c85077d252f3b6cc1d348dec00aa40fe20e
Rodasistemas <rodasistemas@gmail.com>
2019-04-10 00:22:07 -0300

    Estrutura MVC - Adicao de Models e Controllers

---
+++ src/models/user.js
@@ -0,0 +1,32 @@
+const mongoose = require("../database");
+const bcryptjs = require("bcryptjs");
+const UserSchema = mongoose.Schema({
+    name:{
+        type: String,
+        required:true,
+    },
+    email:{
+        type: String,
+        required:true,
+        unique:true,
+        lowercase:true,
+    },
+    password:{
+        type: String,
+        required: true,
+        select: false,
+    },
+    createAt:{
+        type: Date,
+        default: Date.now,
+    },
+});
+
+UserSchema.pre('save', async function(next){
+    const hash = await bcryptjs.hash(this.password,10);
+    this.password = hash;
+});
+
+const User = mongoose.model('User', UserSchema);
+
+module.exports = User;
