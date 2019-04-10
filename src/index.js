7f5ab5b5fdccfd8c707ffae1f48ba207349d3bca
Rodasistemas <rodasistemas@gmail.com>
2019-04-10 18:04:10 -0300

    Modulo Projects e Autenticacao Finalizado

--- src/index.js
+++ src/index.js
@@ -7,5 +7,5 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
 require('./controllers/authController')(app);
-
+require('./controllers/projectController')(app);
 app.listen(3000);
